f07e1605e2b4c8dec53f7943c2fe0835b20d0bb6
Daniel Ajibewa <ajibewadannyboi@gmail.com>
2025-05-31 17:15:34 +0100

    `chore: made some changes to the middlewares`

--- src/middlewares/notfound.middleware.ts
+++ src/middlewares/notfound.middleware.ts
@@ -2,9 +2,10 @@ import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
 export const notFoundError = (
-	req: Request,
-	res: Response,
-	next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) => {
-	res.status(StatusCodes.NOT_FOUND).send(`Route ${req.path} not found`);
+  res.status(StatusCodes.NOT_FOUND).send(`Route ${req.path} not found`);
+  next();
 };
