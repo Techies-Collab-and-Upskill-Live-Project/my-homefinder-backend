de73bc228bd332465839cdd34b4f4879e7765370
Daniel Ajibewa <ajibewadannyboi@gmail.com>
2025-06-14 13:07:33 +0100

    `refactor: refactor auth.controller, passwordReset.service and others and test with postman`

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -1,59 +1,65 @@
-import { Request, Response, NextFunction } from "express";
-import { PrismaClient } from "@prisma/client";
-import { RequestWithUser } from "../interfaces/auth.interface";
-import { StatusCodes } from "http-status-codes";
+import {Request, Response, NextFunction} from "express";
+import {PrismaClient} from "@prisma/client";
+import {RequestWithUser} from "../interfaces/auth.interface";
+import {StatusCodes} from "http-status-codes";
+import {prisma} from "../prisma/prisma";
 
-const prisma = new PrismaClient();
 
 export class MessageController {
-  public sendMessage = async (
-    req: RequestWithUser,
-    res: Response,
-    next: NextFunction
-  ): Promise<void> => {
-    try {
-      const { receiverId, content, propertyId } = req.body;
-      const senderId = req.user.id;
+    private clientPrisma: typeof prisma;
 
-      const message = await prisma.message.create({
-        data: {
-          content,
-          senderId,
-          receiverId,
-          propertyId: propertyId || null,
-        },
-      });
-
-      res.status(StatusCodes.CREATED).json({ message: "Message sent", data: message });
-    } catch (error) {
-      next(error);
+    constructor() {
+        this.clientPrisma = prisma;
     }
-  };
 
-  public getThread = async (
-    req: RequestWithUser,
-    res: Response,
-    next: NextFunction
-  ): Promise<void> => {
-    try {
-      const userId = req.user.id;
-      const withUserId = req.params.withUserId;
+    public sendMessage = async (
+        req: RequestWithUser,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> => {
+        try {
+            const {receiverId, content, propertyId} = req.body;
+            const senderId = req.user.id;
 
-      const messages = await prisma.message.findMany({
-        where: {
-          OR: [
-            { senderId: userId, receiverId: withUserId },
-            { senderId: withUserId, receiverId: userId },
-          ],
-        },
-        orderBy: {
-          createdAt: "asc",
-        },
-      });
+            const message = await this.clientPrisma.message.create({
+                data: {
+                    content,
+                    senderId,
+                    receiverId,
+                    propertyId: propertyId || null,
+                },
+            });
 
-      res.status(StatusCodes.OK).json({ data: messages });
-    } catch (error) {
-      next(error);
-    }
-  };
+            res.status(StatusCodes.CREATED).json({message: "Message sent", data: message});
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    public getThread = async (
+        req: RequestWithUser,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> => {
+        try {
+            const userId = req.user.id;
+            const withUserId = req.params.withUserId;
+
+            const messages = await this.clientPrisma.message.findMany({
+                where: {
+                    OR: [
+                        {senderId: userId, receiverId: withUserId},
+                        {senderId: withUserId, receiverId: userId},
+                    ],
+                },
+                orderBy: {
+                    createdAt: "asc",
+                },
+            });
+
+            res.status(StatusCodes.OK).json({data: messages});
+        } catch (error) {
+            next(error);
+        }
+    };
 }
