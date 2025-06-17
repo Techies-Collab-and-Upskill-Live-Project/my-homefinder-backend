e35dfeb44b7dc322a27bd040a7a79ac0dc8fdcbe
berylatieno <berylatieno30@gmail.com>
2025-06-17 14:24:39 +0300

    feat: review pagination interfaces

--- src/interfaces/review.interface.ts
+++ src/interfaces/review.interface.ts
@@ -23,3 +23,18 @@ export interface ReviewResponse {
   data?: any;
   error?: string;
 }
+
+export interface PaginationParams {
+  page?: number;
+  limit?: number;
+}
+
+export interface PaginatedResponse<T> {
+  data: T[];
+  metadata: {
+    total: number;
+    page: number;
+    limit: number;
+    totalPages: number;
+  };
+}
