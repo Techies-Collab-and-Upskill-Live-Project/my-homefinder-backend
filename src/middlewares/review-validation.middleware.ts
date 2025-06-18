28c88e8814cc320bdbfb96d924249c28240d2434
berylatieno <berylatieno30@gmail.com>
2025-06-18 15:22:42 +0300

    fix: changed max review comment to a const outside class

--- src/middlewares/review-validation.middleware.ts
+++ src/middlewares/review-validation.middleware.ts
@@ -3,8 +3,9 @@ import { StarRating } from '../generated/prisma';
 import HTTPException from '../exceptions/http.exception';
 import { StatusCodes } from 'http-status-codes';
 
+export const MAX_REVIEW_COMMENT_LENGTH = 500;
+
 export class ReviewValidationMiddleware {
-  private static readonly MAX_COMMENT_LENGTH = 1000;
   
   static validateCreateReview(req: Request, res: Response, next: NextFunction): void {
     const { rating, comment } = req.body;
@@ -18,8 +19,8 @@ export class ReviewValidationMiddleware {
     // Validate comment
     if (!comment?.trim()) {
       errors.push('Comment is required');
-    } else if (comment.length > this.MAX_COMMENT_LENGTH) {
-      errors.push(`Comment must be less than ${this.MAX_COMMENT_LENGTH} characters`);
+    } else if (comment.length > MAX_REVIEW_COMMENT_LENGTH) {
+      errors.push(`Comment must be less than ${MAX_REVIEW_COMMENT_LENGTH} characters`);
     }
 
     if (errors.length > 0) {
