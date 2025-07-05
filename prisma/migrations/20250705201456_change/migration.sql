6e678e0b8810ce76cab23233adf8f4d5fa4867f2
Daniel Ajibewa <ajibewadannyboi@gmail.com>
2025-07-05 21:18:00 +0100

    `feat: change the database constarints for the teantProfile and LandlordProfile`

---
+++ prisma/migrations/20250705201456_change/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "LandLordProfile_BVN_key";
+
+-- DropIndex
+DROP INDEX "LandLordProfile_NIN_key";
+
+-- DropIndex
+DROP INDEX "LandLordProfile_driversLicense_key";
+
+-- DropIndex
+DROP INDEX "TenantProfile_NIN_key";
